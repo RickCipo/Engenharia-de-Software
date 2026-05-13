6861dd073ad2c29552d8f406aecf6b2f504701f6
Enrique Cipolla Martins <rickcipolla@gmail.com>
2026-05-13 01:09:33 +0000

    recebimento 1.0

--- recebimentos_mvp/recebimentos_mvp/config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('recebimentos/', include('recebimentos.urls')),
-    path('', lambda r: redirect('recebimentos:dashboard')),
-]
