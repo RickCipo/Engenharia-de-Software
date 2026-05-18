1b828742b1d9fcb905975ffd42a8f1c76c7d3deb
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2026-05-18 12:56:49 -0300

    Update urls.py
    enrique carol pg

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -1,34 +1,10 @@
-"""
-URL configuration for financeiro project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-
 from django.urls import path
 from . import views
-
-app_name = "recebimentos"
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("", views.dashboard, name="dashboard"),
-    path("listar/", views.listar, name="listar"),
-    path("novo/", views.criar, name="criar"),
-    path("<int:pk>/", views.detalhe, name="detalhe"),
-    path("<int:pk>/editar/", views.editar, name="editar"),
-    path("<int:pk>/confirmar/", views.confirmar, name="confirmar"),
-    path("<int:pk>/cancelar/", views.cancelar, name="cancelar"),
-    path("ajax/contratos/", views.contratos_por_centro, name="contratos_ajax"),
+    path('', views.index, name='index'),
+    path('gasto/novo/', views.cadastrar_gasto, name='cadastrar_gasto'),
+    path('recebimento/novo/', views.cadastrar_recebimento, name='cadastrar_recebimento'),
+    path('logs/', views.listar_logs, name='listar_logs'),
 ]
